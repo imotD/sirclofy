5a1481fa60cbe1fdffeaaf80925f7575e288433e
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-02-19 11:06:39 +0700

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-toolbar flat dark>
+      <v-toolbar-title>
+        <v-img max-width="50" src="../assets/logo.png"></v-img>
+      </v-toolbar-title>
+
+      <v-btn text> Now Playing </v-btn>
+
+      <v-btn text> Explore Station </v-btn>
+      <v-spacer></v-spacer>
+      <v-col cols="4">
+        <v-text-field
+          solo
+          hide-details
+          label="Search"
+          append-icon="mdi-magnify"
+        ></v-text-field>
+      </v-col>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
