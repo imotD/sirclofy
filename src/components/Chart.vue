c4dac86121448b4a769dec51482f7fd5e07b8a8a
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-02-20 14:13:43 +0700

    feat(Explor): added page detail chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dashboard mb-5">
+    <v-container>
+      <div class="mb-10">
+        <h2>
+          <v-icon color="blue accent-2 text-capitalize"> mdi-music </v-icon>
+          {{ title }}
+        </h2>
+        <p class="text--secondary">
+          Best <span class="text-lowercase">{{ title }}</span> of the year
+        </p>
+      </div>
+
+      <TopList :items="data" />
+      <div class="text-center">
+        <v-pagination v-model="page" :length="4" circle></v-pagination>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import topTrack from "../../public/track.json";
+import TopList from "@/components/TopList.vue";
+export default {
+  name: "Chart",
+  props: {
+    title: String,
+  },
+  components: {
+    TopList,
+  },
+  data() {
+    return {
+      data: topTrack.tracks.track,
+      transparent: "rgba(255, 255, 255, 0)",
+      page: 1,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/ExplorChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mt-16">
+    <Navbar />
+    <Banner />
+    <Chart :title="title" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Banner from "@/components/Banner.vue";
+import Chart from "@/components/Chart.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "ExplorChart",
+  components: {
+    Navbar,
+    Banner,
+    Chart,
+    Footer,
+  },
+  data() {
+    return {
+      title: null,
+    };
+  },
+  created() {
+    this.title = this.$route.params.type;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
